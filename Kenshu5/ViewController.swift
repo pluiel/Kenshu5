4c6c51976e4d635adeabb2067b51924761905885
pluiel <pluiel@outlook.com>
2020-05-24 10:41:30 +0900

    Initial Commit

---
+++ Kenshu5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kenshu5
+//
+//  Created by Yuka Uematsu on 2020/05/24.
+//  Copyright © 2020 Uematsu Well. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
